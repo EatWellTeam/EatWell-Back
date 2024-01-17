8ab5c43e53799049aed95aaa53532b315a6257b5
Dolev <dolevs1890@gmail.com>
2024-01-17 20:52:09 +0200

    changed controller of comment to be part of the base controller

--- src/controllers/comment_controller.ts
+++ src/controllers/comment_controller.ts
@@ -1,43 +1,50 @@
-import {Request, Response} from "express";
-import Post from "../models/post_model";
-import Comment from "../models/comments_model";
+// import {Request, Response} from "express";
+// import Post from "../models/post_model";
+// import Comment from "../models/comments_model";
 
 
-const createComment = async (req: Request, res: Response) => {
-  console.log("------CREATE COMMENT------");
-  const { user, post, body } = req.body;
-  try {
-    const newComment = await Comment.create({
-      user,
-      post,
-      body,
-    });
-    await Post.findByIdAndUpdate(post, {
-      $push: { comments: newComment._id },
-    });
-    res.status(200).json(newComment);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-}
-const getCommentById = async (req: Request, res: Response) => {
-  console.log("------GET COMMENT BY ID------");
-  const  postId  = req.params.id;
-  const commentId = req.params.commentId;
-  try {
-   // Fetch the post and populate the comments
-   const post = await Post.findById(postId).populate('comments');
+// const createComment = async (req: Request, res: Response) => {
+//   console.log("------CREATE COMMENT------");
+//   const { user, post, body } = req.body;
+//   try {
+//     const newComment = await Comment.create({
+//       user,
+//       post,
+//       body,
+//     });
+//     await Post.findByIdAndUpdate(post, {
+//       $push: { comments: newComment._id },
+//     });
+//     res.status(200).json(newComment);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// }
+// const getCommentById = async (req: Request, res: Response) => {
+//   console.log("------GET COMMENT BY ID------");
+//   const  postId  = req.params.id;
+//   const commentId = req.params.commentId;
+//   try {
+//    // Fetch the post and populate the comments
+//    const post = await Post.findById(postId).populate('comments');
 
-   // Find the specific comment
-   const comment = post.comments.find(comment => comment._id.toString() === commentId);
-   if(comment){
-      res.status(200).json(comment);
-   }
-    else{
-        res.status(404).json("Comment not found");
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-}
-export default { createComment, getCommentById };
+//    // Find the specific comment
+//    const comment = post.comments.find(comment => comment._id.toString() === commentId);
+//    if(comment){
+//       res.status(200).json(comment);
+//    }
+//     else{
+//         res.status(404).json("Comment not found");
+//     }
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// }
+// export default { createComment, getCommentById };
+
+import CommentModel, { IComment } from "../models/comments_model";
+import createController from "./base_controller";
+
+const CommentsController = createController<IComment>(CommentModel);
+
+export default CommentsController;
