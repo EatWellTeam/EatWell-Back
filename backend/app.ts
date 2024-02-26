edddfbf2ccfd98484ec06065e884b143403e3b8f
Arik <arik.l@hiredscore.com>
2024-02-26 18:07:36 +0200

    added cors allow rules

--- backend/app.ts
+++ backend/app.ts
@@ -19,6 +19,18 @@ const initApp = (): Promise<Express> => {
     mongoose.connect(url!).then(() => {
       const app = express();
 
+      app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+        res.header(
+          "Access-Control-Allow-Headers",
+          "Origin, X-Requested-With, Content-Type, Accept"
+        );
+        res.header(
+          "Access-Control-Allow-Methods",
+          "GET, POST, PUT, DELETE, OPTIONS"
+        );
+        next();
+      });
       app.use(bodyParser.json());
       app.use(bodyParser.urlencoded({ extended: true }));
       app.use("/auth", authRoute);
