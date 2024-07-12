5e3590b7f32983cf5d0abb40f1263fa7f92526e8
Dolev <dolevs1890@gmail.com>
2024-07-12 11:41:12 +0300

    using open ai version 1

--- backend/tests/openai.test.ts
+++ backend/tests/openai.test.ts
@@ -1,12 +1,35 @@
 // openAI.test.ts
-
 import env from "dotenv";
+import path from "path";
+import fs from "fs";
+import { fetchChatCompletion, Message } from "../../services/openAI";
 env.config();
-import { continueConversation } from "../../services/openAI";
+const filePath = path.join(__dirname, "../public/images.jpeg");
+console.log("imageURL", filePath);
+const messages: Message[] = [
+  {
+    role: "user",
+    content:
+      "Could you write the name of each item and their quantity in the following picture please?",
+    image_url: filePath,
+  },
+  {
+    role: "assistant",
+    content: "",
+  },
+];
+
 describe("fetchChatCompletion", () => {
   it("fetches chat completion successfully", async () => {
-    const response = await continueConversation();
-    expect(response).toBeDefined();
-    console.log("response: ", response);
-  });
+    console.log("messages", messages);
+    const imageExists = fs.existsSync(filePath);
+    expect(imageExists).toBe(true);
+    try {
+      const response = await fetchChatCompletion(messages);
+      expect(response).toBeDefined();
+      console.log("response", response);
+    } catch (error) {
+      console.error("API Error: ", error.message);
+    }
+  }, 30000); // Increase timeout to 30 seconds
 });
--- services/openAI.ts
+++ services/openAI.ts
@@ -1,42 +1,68 @@
 // openAI.ts
 import OpenAI from "openai";
+import dotenv from "dotenv";
+import readline from "readline";
+import path from "path";
+import fs from "fs";
+dotenv.config();
 
 export interface Message {
   role: "user" | "assistant";
-  content: string;
+  content?: string;
+  image_url?: string;
 }
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+const userInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function promptUser() {
+  userInterface.prompt();
+  userInterface.on("line", async (input) => {
+    const fileExists = fs.existsSync(path.join(__dirname, input));
+    const isImageUrl = fileExists && input.includes(".jpeg");
+    const messages: Message[] = [
+      {
+        role: "user",
+        content: isImageUrl ? undefined : input,
+        image_url: isImageUrl ? path.join(__dirname, input) : undefined,
+      },
+      {
+        role: "assistant",
+        content: "",
+      },
+    ];
+
+    try {
+      const response = await fetchChatCompletion(messages);
+      console.log("ChatGPT:", response);
+      promptUser(); // Loop back to continue the conversation
+    } catch (error) {
+      console.error("API Error: ", error.message);
+      userInterface.close();
+    }
+  });
+}
+
 async function fetchChatCompletion(messages: Message[]): Promise<string> {
+  const formattedMessages = messages.map((message) => {
+    return {
+      role: message.role,
+      content: message.content,
+      image: message.image_url,
+    };
+  });
   const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: messages,
+    model: "gpt-4o",
+    messages: formattedMessages,
   });
   const latestResponse = response.choices[0].message.content;
-  console.log(latestResponse);
+  // console.log("latestResponse", latestResponse);
   return latestResponse;
 }
-
-// Example usage
-async function continueConversation() {
-  const conversationHistory: Message[] = [
-    { role: "user", content: "Say this is a test" },
-  ];
-
-  // Initial call
-  let response = await fetchChatCompletion(conversationHistory);
-  conversationHistory.push({ role: "assistant", content: response });
-
-  // Continue the conversation
-  conversationHistory.push({
-    role: "user",
-    content: "How about another question?",
-  });
-  response = await fetchChatCompletion(conversationHistory);
-  conversationHistory.push({ role: "assistant", content: response });
-}
-
-export { fetchChatCompletion, continueConversation };
+export { promptUser, fetchChatCompletion };
